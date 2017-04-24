0d86df672727169c4326d8260fd05d493ac9e65b
bojant987 <bojant987@hotmail.com>
2017-04-24 22:36:42 +0200

    header done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+// navbar scroll
+$(document).scroll(function() {
+  if ($(document).scrollTop() > 10) {
+    $(".navbar-default").addClass("scroll-navbar");
+    $(".navbar-brand img").attr("src", "img/logo-small.png");
+    $(".navbar-brand").addClass("scroll-brand");
+    $(".scroll-number").css("display", "inline-block");
+    $(".navbar-default .signup a, .navbar-default .lang > a").addClass("scroll-btn");
+  } else {
+    $(".navbar-default").removeClass("scroll-navbar");
+    $(".navbar-brand img").attr("src", "img/logo.png");
+    $(".navbar-brand").removeClass("scroll-brand");
+    $(".scroll-number").css("display", "none");
+    $(".navbar-default .signup a, .navbar-default .lang > a").removeClass("scroll-btn");
+  }
+});
