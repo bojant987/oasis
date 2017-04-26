690727503cf7afcbc1809092a34fde36273efd6b
bojant987 <bojant987@hotmail.com>
2017-04-26 23:56:07 +0200

    index page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,16 +1,32 @@
-// navbar scroll
-$(document).scroll(function() {
-  if ($(document).scrollTop() > 10) {
-    $(".navbar-default").addClass("scroll-navbar");
-    $(".navbar-brand img").attr("src", "img/logo-small.png");
-    $(".navbar-brand").addClass("scroll-brand");
-    $(".scroll-number").css("display", "inline-block");
-    $(".navbar-default .signup a, .navbar-default .lang > a").addClass("scroll-btn");
-  } else {
-    $(".navbar-default").removeClass("scroll-navbar");
-    $(".navbar-brand img").attr("src", "img/logo.png");
-    $(".navbar-brand").removeClass("scroll-brand");
-    $(".scroll-number").css("display", "none");
-    $(".navbar-default .signup a, .navbar-default .lang > a").removeClass("scroll-btn");
-  }
+$(document).ready(function() {
+
+  // navbar scroll
+  $(document).scroll(function() {
+    if ($(document).scrollTop() > 1) {
+
+      $(".navbar-default").addClass("scroll-navbar");
+      $(".navbar-brand img").attr("src", "img/logo-small.png");
+      $(".navbar-brand").addClass("scroll-brand");
+      $(".scroll-number").css("display", "inline-block");
+      $(".navbar-default .signup a, .navbar-default .lang > a").addClass("scroll-btn");
+
+    } else {
+
+      $(".navbar-default").removeClass("scroll-navbar");
+      $(".navbar-brand img").attr("src", "img/logo.png");
+      $(".navbar-brand").removeClass("scroll-brand");
+      $(".scroll-number").css("display", "none");
+      $(".navbar-default .signup a, .navbar-default .lang > a").removeClass("scroll-btn");
+
+    }
+  });
+
+  // bx slider
+  $('.banner-slider').bxSlider({
+    controls: false
+  });
+
 });
+
+// set line width to h1 width
+$(".contact-banner .line").width($(".contact-banner h1").width());
