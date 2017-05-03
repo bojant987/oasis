c86397b6be557479ea28965c781407bf57c17aa5
bojant987 <bojant987@hotmail.com>
2017-05-03 19:02:55 +0200

    why us done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -59,23 +59,93 @@ $(document).ready(function() {
 
   $(".close-content").click(function(e) {
     e.preventDefault();
-    $(".more-content").fadeOut();
+    $(this).parent().parent().fadeOut();
   });
 
   // nav tabs img swap on active
-  $(".nav-tabs li.active img").attr("src", $(".nav-tabs li.active img").attr("src").replace(".png", "-hov.png"));
+  if ($(".nav-tabs li.active img").length > 0) {
+    $(".nav-tabs li.active img").attr("src", $(".nav-tabs li.active img").attr("src").replace(".png", "-hov.png"));
+  }
 
-  $(".nav-tabs li").click(function() {
+  $(".nav-tabs").on("click", "li", function(e) {
     var orgSrc = $(this).find("img").attr("src");
     var newSrc = orgSrc.replace(".png", "-hov.png");
+    var imgs = $(".nav-tabs li img");
 
-    document.querySelectorAll(".nav-tabs li img").forEach(function(img) {
-      if (img.src.search("hov") !== -1) {
-        img.src = img.src.replace("-hov.png", ".png");
+
+    imgs.each(function() {
+      if ($(this).attr("src").search("hov") !== -1 && $(this).get(0) !== $(e.target).get(0) && $(this).parent().get(0) !== $(e.target).get(0)) {
+        $(this).attr("src", $(this).attr("src").replace("-hov.png", ".png"));
       }
     });
 
-    $(this).find("img").attr("src", newSrc);
+    if (!$(this).hasClass("active")) {
+      $(this).find("img").attr("src", newSrc);
+    }
+  });
+
+  // input for circle progress
+  function processInputForProgressBar(circle) {
+    var min = circle.attr("data-min");
+    var max = circle.attr("data-max");
+    var value = circle.attr("data-value");
+
+    return value / (max - min);
+  }
+
+  // label for circle progress
+  function labelForProgressBar() {
+    $(".label-circle").each(function() {
+      var value = $(this).prev().attr("data-value");
+      $(this).html(value + " days");
+    });
+  }
+  labelForProgressBar();
+
+  // circle progress bars
+  $("#progress-circle1").circleProgress({
+    value: processInputForProgressBar($("#progress-circle1")),
+    size: 200,
+    thickness: 35,
+    fill: {
+      color: "#54acde"
+    },
+    emptyFill: "#d5d5d5"
+  });
+
+  $("#progress-circle2").circleProgress({
+    value: processInputForProgressBar($("#progress-circle2")),
+    size: 150,
+    thickness: 25,
+    fill: {
+      color: "#54acde"
+    },
+    emptyFill: "#d5d5d5"
+  });
+
+  $("#progress-circle3").circleProgress({
+    value: processInputForProgressBar($("#progress-circle3")),
+    size: 150,
+    thickness: 25,
+    fill: {
+      color: "#54acde"
+    },
+    emptyFill: "#d5d5d5"
+  });
+
+  // opened-link on why-us page
+  $(".circle-wrapper").on("click", "a", function(e) {
+    e.preventDefault();
+    var openedLinks = $(".opened-link");
+    var id = $(this).attr("data-opens");
+
+
+    openedLinks.each(function() {
+        $(this).hide();
+    });
+
+    $(".why-oasis").find("#" + id).fadeIn();
+
   });
 
 });
